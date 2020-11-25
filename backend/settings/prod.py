2459ef318e2045bd27482d8a1f56ee70bee9c06a
gustutu <augustin.turrta@gmail.com>
2020-11-25 20:52:18 +0100

    prod.py modifications for deployement

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,17 +1,46 @@
 """ Production Settings """
 
 import os
-import dj_database_url
+import sys
+from urllib.parse import urlparse
 from .dev import *
+# urlparse.uses_netloc.append('mysql')
 
+try:
+
+    # Check to make sure DATABASES is set in settings.py file.
+    # If not default to {}
+
+    if 'DATABASES' not in locals():
+        DATABASES = {}
+
+    if 'DATABASE_URL' in os.environ:
+        url = urlparse(os.environ['DATABASE_URL'])
+
+        # Ensure default database exists.
+        DATABASES['default'] = DATABASES.get('default', {})
+
+        # Update with environment configuration.
+        DATABASES['default'].update({
+            'NAME': url.path[1:],
+            'USER': url.username,
+            'PASSWORD': url.password,
+            'HOST': url.hostname,
+            'PORT': url.port,
+        })
+
+        if url.scheme == 'mysql':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+except Exception:
+    print('Unexpected error:', sys.exc_info())
 ############
 # DATABASE #
 ############
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL')
+#     )
+# }
 
 
 ############
@@ -20,7 +49,7 @@ DATABASES = {
 
 DEBUG = bool(os.getenv('DJANGO_DEBUG', ''))
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # Set to your Domain here (eg. 'django-vue-template-demo.herokuapp.com')
 ALLOWED_HOSTS = ['*']
