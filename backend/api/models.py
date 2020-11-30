4204d8af9b35b7270f270da57639d4c9d493debc
gustutu <augustin.turrta@gmail.com>
2020-11-30 14:41:43 +0100

    remove unused cat model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,21 +1,8 @@
 from rest_framework import serializers
 from django.db import models
 
-# le model Cat existe pour conserver les images en base de données et 
-# délégué la gestion de la table a Django
-
 
 class Cat(models.Model):
-    id = models.CharField(primary_key=True, max_length=20)
-    image = models.BinaryField()
-    score = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'cats'
-
-
-class CatNoImage(models.Model):
     id = models.CharField(primary_key=True, max_length=20)
     score = models.IntegerField()
 
@@ -24,13 +11,9 @@ class CatNoImage(models.Model):
         db_table = 'cats'
 
 
-class CatNoImageSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = CatNoImage
-        fields = ('id', 'score')
-
-
 class CatSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cat
-        fields = ('id', 'image', 'score')
+        fields = ('id', 'score')
+
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
 from rest_framework import viewsets
 from rest_framework.views import APIView
-from .models import Cat, CatNoImage, CatNoImageSerializer, CatSerializer
+from .models import  CatNoImage, CatNoImageSerializer
 from django.shortcuts import get_object_or_404
 import random
 from rest_framework.response import Response
@@ -62,58 +62,3 @@ class Cutest(viewsets.ViewSet):
 
 
 
-# class CatView(viewsets.ViewSet):
-#     """
-#     API endpoint to get two different  random cats
-#     """
-
-#     def list(self, request):
-#         queryset = CatNoImage.objects.all()
-#         all_cats = CatNoImage.objects.all()
-#         serializer = CatNoImageSerializer(all_cats, many=True)
-#         return Response(serializer.data)
-
-#     def retrieve(self, request, pk=None):
-#         queryset = CatNoImage.objects.all()
-#         # cats = []
-#         # TODO: move to dja,go init
-#         my_ids = CatNoImage.objects.values_list('id', flat=True)
-#         my_ids = list(my_ids)
-#         rand_ids = random.sample(my_ids, 2)
-#         two_random_cat = CatNoImage.objects.filter(id__in=rand_ids)
-
-#         serializer = CatNoImageSerializer(two_random_cat, many=True)
-
-#         return Response(serializer.data)
-
-#     def create(self, request):
-        
-#         parsed_body = json.loads(request.body)
-#         print(request.body)
-#         cat1_id = parsed_body["catsIds"][0]
-#         cat2_id = parsed_body["catsIds"][1]
-#         winner_id = parsed_body["winnerId"]
-
-#         cat1 = CatNoImage.objects.get(id=cat1_id)
-#         cat2 = CatNoImage.objects.get(id=cat2_id)
-
-#         if cat1.id == winner_id:
-#             winner_cat = cat1
-#             looser_cat = cat2
-#         elif cat2.id == winner_id:
-#             winner_cat = cat2
-#             looser_cat = cat1
-#         else:
-#             # TODO: error response code
-#             print("winner not in cat Ids")
-
-#         winner_score, looser_score = computeElo.compute_new_scores(
-#             winner_cat.score, looser_cat.score)
-
-#         winner_cat.score = winner_score
-#         looser_cat.score = looser_score
-
-#         winner_cat.save()
-#         looser_cat.save()
-
-#         return Response()
