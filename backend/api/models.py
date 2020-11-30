822ec64a80433723710b64df715aa3c6a3c3e85d
gustutu <augustin.turrta@gmail.com>
2020-11-30 14:41:43 +0100

    remove unused cat model

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,17 +4,6 @@ from django.db import models
 # le model Cat existe pour conserver les images en base de données et 
 # délégué la gestion de la table a Django
 
-
-class Cat(models.Model):
-    id = models.CharField(primary_key=True, max_length=20)
-    image = models.BinaryField()
-    score = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'cats'
-
-
 class CatNoImage(models.Model):
     id = models.CharField(primary_key=True, max_length=20)
     score = models.IntegerField()
@@ -30,7 +19,3 @@ class CatNoImageSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'score')
 
 
-class CatSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Cat
-        fields = ('id', 'image', 'score')
